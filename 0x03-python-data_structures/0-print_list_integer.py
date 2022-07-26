caad910221e006cd60384d50d26989134df278d1
Emmanuela-ikpontu <ikpontuemmanuela@gmail.com>
2022-07-26 05:43:36 -0700

    0-print_list_integer.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 
-def square_matrix_simple(matrix=[]):
-    """Compute the square value of all integers of a matrix."""
-    return ([list(map(lambda x: x * x, row)) for row in matrix])
+def print_list_integer(my_list=[]):
+    """
+    A function that prints all integers of a list.
+    """
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
